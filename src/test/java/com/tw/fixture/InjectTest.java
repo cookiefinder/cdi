b0edab8febba30b99a5306143d690b719675dcb4
Zijie Jiang <zijie.jiang@thoughtworks.com>
2021-09-07 16:10:03 +0800

    test inject for field inject

--- src/test/java/com/tw/fixture/InjectTest.java
+++ src/test/java/com/tw/fixture/InjectTest.java
@@ -4,6 +4,7 @@ import com.thoughtworks.fusheng.integration.junit5.FuShengTest;
 import com.tw.FushengContainer;
 import com.tw.fixture.model.test.Computer;
 import java.util.List;
+import java.util.Objects;
 
 @FuShengTest
 public class InjectTest {
@@ -19,5 +20,12 @@ public class InjectTest {
         List<?> component = fushengContainer.getComponent(clazz);
         Computer computer = (Computer) component.get(0);
         return computer.print();
+    }
+
+    public Boolean checkDependencyExist(String className) throws ClassNotFoundException {
+        Class<?> clazz = Class.forName(className);
+        List<?> component = fushengContainer.getComponent(clazz);
+        Computer computer = (Computer) component.get(0);
+        return Objects.nonNull(computer.getKeyboard());
     }
 }
--- src/test/java/com/tw/fixture/model/test/Computer.java
+++ src/test/java/com/tw/fixture/model/test/Computer.java
@@ -3,14 +3,24 @@ package com.tw.fixture.model.test;
 import javax.inject.Inject;
 
 public class Computer {
-    private final Console console;
+    private Console console;
+    @Inject
+    private Keyboard keyboard;
 
     @Inject
     public Computer(Console console) {
         this.console = console;
     }
 
+    public Computer(Keyboard keyboard) {
+        this.keyboard = keyboard;
+    }
+
     public String print() {
         return console.print();
+    }
+
+    public Keyboard getKeyboard() {
+        return keyboard;
     }
 }
